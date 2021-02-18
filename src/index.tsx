5b6a4bf2473c92825a3f88f9710f9994141f35cf
Ben Partridge <ben.partridge249@gmail.com>
2021-02-18 18:10:14 +1100

    Initial project layout

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  text-align: center;
+`
+
+export const Hello: FunctionComponent = () => <Container>Hello, world</Container>
