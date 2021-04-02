e8463eeee88cab52f0294640da0a6c44bb971f69
Ben Partridge <ben.partridge249@gmail.com>
2021-04-02 17:23:50 +1100

    Add a social for linked in

--- src/components/socials/Socials.tsx
+++ src/components/socials/Socials.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import styled from 'styled-components'
-import { TwitterSocial, GitHubSocial, ItchSocial, SocialProvidedProps } from './Social'
+import { GitHubSocial, ItchSocial, LinkedInProps, LinkedInSocial, SocialProvidedProps, TwitterSocial } from './Social'
 
 const SocialsContainer = styled.div`
   display: flex;
@@ -23,16 +23,18 @@ export type SocialsProps = {
   githubProps?: SocialProvidedProps
   itchIoProps?: SocialProvidedProps
   twitterProps?: SocialProvidedProps
+  linkedInProps?: LinkedInProps
 }
 
-export const Socials: FunctionComponent<SocialsProps> = ({ githubProps, itchIoProps, twitterProps }) => {
-  if (githubProps || itchIoProps || twitterProps) {
+export const Socials: FunctionComponent<SocialsProps> = ({ githubProps, itchIoProps, twitterProps, linkedInProps }) => {
+  if (githubProps || itchIoProps || twitterProps || linkedInProps) {
     return (
       <SocialsContainer>
         <SocialsHeader>Socials</SocialsHeader>
-        {twitterProps && <TwitterSocial handle={twitterProps.handle} iconUrl={twitterProps.iconUrl} />}
-        {githubProps && <GitHubSocial handle={githubProps.handle} iconUrl={githubProps.iconUrl} />}
-        {itchIoProps && <ItchSocial handle={itchIoProps.handle} iconUrl={itchIoProps.iconUrl} />}
+        {twitterProps && <TwitterSocial {...twitterProps} />}
+        {githubProps && <GitHubSocial {...githubProps} />}
+        {itchIoProps && <ItchSocial {...itchIoProps} />}
+        {linkedInProps && <LinkedInSocial {...linkedInProps} />}
       </SocialsContainer>
     )
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,28 @@
-import { usePageViewEvent } from './components/analytics/hooks'
 import { AnalyticsScript } from './components/analytics/AnalyticsScript'
-export { AnalyticsScript, usePageViewEvent }
-
-import { Card, BaseCardProps, CardProps } from './components/cards/Card'
+import { usePageViewEvent } from './components/analytics/hooks'
+import { BaseCardProps, Card, CardProps } from './components/cards/Card'
 import { LargeCard } from './components/cards/LargeCard'
-export { Card, LargeCard, BaseCardProps, CardProps }
-
-import Layout, { LayoutProps, ScaledFontLayout } from './components/layout/MainLayout'
-import { Header, HeaderProps, HeaderNavigationButtonProps } from './components/layout/Header'
-import { Footer } from './components/layout/Footer'
-export { Footer, Header, HeaderProps, HeaderNavigationButtonProps, Layout, LayoutProps, ScaledFontLayout }
-
-import { Socials } from './components/socials/Socials'
-import { GitHubSocial, ItchSocial, TwitterSocial, SocialProps } from './components/socials/Social'
-export { GitHubSocial, ItchSocial, TwitterSocial, Socials, SocialProps }
-
-import { PiffedTheme } from './components/theme/Theme'
-export { PiffedTheme }
-
 import { Paragraph, Title } from './components/Content'
-export { Paragraph, Title }
-
+import { Footer } from './components/layout/Footer'
+import { Header, HeaderNavigationButtonProps, HeaderProps } from './components/layout/Header'
+import Layout, { LayoutProps, ScaledFontLayout } from './components/layout/MainLayout'
 import { HyperLink, HyperLinkProps } from './components/Link'
-export { HyperLink, HyperLinkProps }
-
 import { Loader } from './components/Loader'
+import {
+  CustomSocial,
+  GitHubSocial,
+  ItchSocial,
+  LinkedInSocial,
+  SocialProps,
+  TwitterSocial,
+} from './components/socials/Social'
+import { Socials } from './components/socials/Socials'
+import { PiffedTheme } from './components/theme/Theme'
+export { AnalyticsScript, usePageViewEvent }
+export { Card, LargeCard, BaseCardProps, CardProps }
+export { Footer, Header, HeaderProps, HeaderNavigationButtonProps, Layout, LayoutProps, ScaledFontLayout }
+export { CustomSocial, LinkedInSocial, GitHubSocial, ItchSocial, TwitterSocial, Socials, SocialProps }
+export { PiffedTheme }
+export { Paragraph, Title }
+export { HyperLink, HyperLinkProps }
 export { Loader }
